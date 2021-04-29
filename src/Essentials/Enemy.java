b34ff196bfad489ea1212202a7700ae4cfe0e03d
gabri00 <gibby.nicchia@gmail.com>
2021-04-29 11:19:46 +0200

    update

--- src/Essentials/Enemy.java
+++ src/Essentials/Enemy.java
@@ -1,9 +1,11 @@
 package Essentials;
 
+import java.util.Random;
+
 public class Enemy extends MainStats {
 
     private final int danger;       // 1: slimes and goblins, 2: ogre and demons, 3: archdemons and dragons
-    private int expDrop;
+    private final int expDrop;
 
     public Enemy(String tag, double hp, double atk, double def, double crit, int danger, int expDrop) {
         super(tag, hp, atk, def, crit);
@@ -13,6 +15,16 @@ public class Enemy extends MainStats {
 
     public void hit(Hero he) {
         he.setHp(he.getHp() - (this.getAtk() - he.getDef()));
+    }
+
+    public void dropArtifact() {
+        Random rand = new Random();
+
+        int dropProb = 30 * danger;     // 30% * danger
+
+        if (rand.nextInt(101) <= dropProb) {
+            // Drop a random item based on the level of the enemy
+        }
     }
 
 }
--- src/Essentials/Hero.java
+++ src/Essentials/Hero.java
@@ -1,17 +1,51 @@
 package Essentials;
 
+import static java.lang.Math.floor;
+
 public class Hero extends MainStats {
 
-    public Weapon equippedWeapon;
+    private Weapon equippedWeapon;
     private int level = 1;
-    private int exp = 0;
+    private int exp = 1;
 
     public Hero(String tag, double hp, double atk, double def, double crit) {
         super(tag, hp, atk, def, crit);
     }
 
-    public void tryLevelUp() {
-        // Gaining exp algorithm to implement
+    public Weapon getEquippedWeapon() {
+        return equippedWeapon;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    private void setEquippedWeapon(final Weapon w) {
+        equippedWeapon = w;
+    }
+
+    private void setExp(final int exp) {
+        this.exp = exp;
+    }
+
+    public void addExp(final int exp) {
+        final int maxExp = level * 50;
+
+        setExp(getExp() + exp);
+
+        if (getExp() >= maxExp) {
+            levelUp();
+
+            setExp((getExp() > maxExp) ? (getExp() - maxExp) : 0);
+        }
+    }
+
+    private void levelUp() {
+        level++;
+        setHp(floor(getHp() * 1.5));
+        setAtk(getAtk() + 20);
+        setDef(getDef() + 5);
+        setCrit(getCrit());
     }
 
     public void hit(Enemy en) {
@@ -19,24 +53,25 @@ public class Hero extends MainStats {
     }
 
     public void reinforces() {
-        double def_percentage = this.getDef() * 0.05;    // Increment def by 5%
-        this.setDef(this.getDef() + def_percentage);
+        this.setDef(this.getDef() + floor(this.getDef() * 0.05));  // Increment def by 5%
     }
 
-    public void equipWeapon(Weapon w) {
-        equippedWeapon = w;
+    public void equipWeapon(final Weapon w) {
+        setEquippedWeapon(w);
 
-        if (w.getAtk() != 0.0)    this.setAtk(this.getAtk() + w.getAtk());
-        if (w.getCrit() != 0.0)   this.setCrit(this.getCrit() + w.getCrit());
-        if (w.getHp() != 0.0)     this.setHp(this.getHp() + w.getHp());
-        if (w.getDef() != 0.0)    this.setDef(this.getDef() + w.getDef());
+        if (w.getHp() != 0)     this.setHp(this.getHp() + w.getHp());
+        if (w.getAtk() != 0)    this.setAtk(this.getAtk() + w.getAtk());
+        if (w.getDef() != 0)    this.setDef(this.getDef() + w.getDef());
+        if (w.getCrit() != 0)   this.setCrit(this.getCrit() + w.getCrit());
     }
 
     public void unequipWeapon() {
-        if (equippedWeapon.getAtk() != 0.0)    this.setAtk(this.getAtk() - equippedWeapon.getAtk());
-        if (equippedWeapon.getCrit() != 0.0)   this.setCrit(this.getCrit() - equippedWeapon.getCrit());
-        if (equippedWeapon.getHp() != 0.0)     this.setHp(this.getHp() - equippedWeapon.getHp());
-        if (equippedWeapon.getDef() != 0.0)    this.setDef(this.getDef() - equippedWeapon.getDef());
+        if (equippedWeapon.getHp() != 0)     this.setHp(this.getHp() - equippedWeapon.getHp());
+        if (equippedWeapon.getAtk() != 0)    this.setAtk(this.getAtk() - equippedWeapon.getAtk());
+        if (equippedWeapon.getDef() != 0)    this.setDef(this.getDef() - equippedWeapon.getDef());
+        if (equippedWeapon.getCrit() != 0)   this.setCrit(this.getCrit() - equippedWeapon.getCrit());
+
+        setEquippedWeapon(null);
     }
 
 }
