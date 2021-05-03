0e5d21277cf7eec3ab1b438f87acdbc6a1cbc9a7
gabri00 <gibby.nicchia@gmail.com>
2021-05-03 11:18:27 +0200

    minor update - 2

--- src/Core/Game.java
+++ src/Core/Game.java
@@ -74,48 +74,6 @@ public class Game implements java.io.Serializable {
     }
 
 
-    // Just print some basic instructions when the game starts
-    public void initGame(final String[] args) {
-        if (args.length > 0) {
-            if (args[0].equals("-h") || args[0].equals("--help")) {
-                System.out.println("""
-                        Some basic guidelines for the game:
-                        At the very beginning you will have to choose a class for your hero, you can choose between:
-                            - archer hero
-                            - sword hero
-                            - spear hero
-                        Every hero begins with different stats and it also gets an upgrade based on the chosen weapon.
-                        
-                        After the hero selection you will begin the exploration of the dungeon, the latter will have n floors with 3 chambers for each floor.
-                        In each room you will find an enemy waiting for you. Of course the fist floors contain easier monster, such as slimes and goblins.
-                        Then, lower in the dungeon, you will encounter stronger enemies like ogre and demons.
-                        If you've passed even this levels then you can go even lower in the dungeon and find incredibly strong monsters, such as archdemons and dragons!
-                        
-                        Each enemy will drop experience (useful for leveling up your character), and maybe even an artifact.
-                        Artifacts can be used to upgrade your hero's stats to become even stronger!
-                        There are 3 types of artifacts:
-                            - armlets (can upgrade ATK, DEF and CR)
-                            - rings (can upgrade ATK and CR)
-                            - necklets (can upgrade HP and DEF)
-                        
-                        >> Please note that at the current version of the game artifacts and weapons cannot be upgraded <<
-                        
-                        At the end of each fight the game progress will be saved in a log file, so you can resume the exploration whenever you want.
-                        
-                        Enjoy this simple game and please don't be afraid of making pull requests to to the github repo :)
-                        """);
-            }
-            else System.out.println("Argument " + args[0] + " is not valid");
-
-            System.exit(0);
-        }
-        else System.out.print("""
-                Welcome to the dungeon! Let's begin your adventure...
-                Please increase the console size for a better gaming experience!
-                """);
-    }
-
-
     // Create the dungeon matrix and assign an enemy to each chamber
     public void generateDungeon() {
         final var enemies = mapEnemies();
@@ -204,9 +162,7 @@ public class Game implements java.io.Serializable {
 
         if (currFloor == 1) System.out.println("Welcome to the first floor, chamber " + currChamber);
         else if (currFloor == nFloors) System.out.println("Welcome to the last floor, chamber " + currChamber);
-        else System.out.printf("Welcome to floor %d, chamber %d%n", currFloor, currChamber);
-
-        System.out.println();
+        else System.out.printf("\nWelcome to floor %d, chamber %d%n", currFloor, currChamber);
 
         fight(currFloor, currChamber >= 3 ? currChamber - 1 : currChamber);
 
@@ -269,7 +225,7 @@ public class Game implements java.io.Serializable {
             oos.writeObject(game);
             oos.flush();
             oos.close();
-            System.out.println("Game saved!");
+            System.out.println("Game saved!\n");
         } catch (IOException e) {
             System.out.println("Serialization error!\n" + e.getClass() + ": " + e.getMessage());
         }
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,60 @@ import java.io.IOException;
 
 public class Main {
 
+    // Prints some basic instructions
+    public static void printInstructions() {
+        System.out.println("""
+                Some basic guidelines for the game:
+                At the very beginning you will have to choose a class for your hero, you can choose between:
+                    - archer hero
+                    - sword hero
+                    - spear hero
+                Every hero begins with different stats and it also gets an upgrade based on the chosen weapon.
+                
+                After the hero selection you will begin the exploration of the dungeon, the latter will have n floors with 3 chambers for each floor.
+                In each room you will find an enemy waiting for you. Of course the fist floors contain easier monster, such as slimes and goblins.
+                Then, lower in the dungeon, you will encounter stronger enemies like ogre and demons.
+                If you've passed even this levels then you can go even lower in the dungeon and find incredibly strong monsters, such as archdemons and dragons!
+                
+                Each enemy will drop experience (useful for leveling up your character), and maybe even an artifact.
+                Artifacts can be used to upgrade your hero's stats to become even stronger!
+                There are 3 types of artifacts:
+                    - armlets (can upgrade ATK, DEF and CR)
+                    - rings (can upgrade ATK and CR)
+                    - necklets (can upgrade HP and DEF)
+                
+                >> Please note that at the current version of the game artifacts and weapons cannot be upgraded <<
+                
+                At the end of each fight the game progress will be saved in a log file, so you can resume the exploration whenever you want.
+                
+                Enjoy this simple game and please don't be afraid of making pull requests to to the github repo :)
+                """);
+    }
+
+    // Runs the game and checks for arguments
     public static void main(String[] args) throws IOException {
         Game game = new Game();
 
-        if (args.length > 0 && (args[0].equals("-l") || args[0].equals("--load")))
-            game = game.loadGame();
+        if (args.length > 0) {
+            if (args[0].equals("-l") || args[0].equals("--load"))
+                game = game.loadGame();
+            else {
+                if (args[0].equals("-h") || args[0].equals("--help"))
+                    printInstructions();
+                else System.out.println("Argument " + args[0] + " doesn't exist");
+
+                System.exit(0);
+            }
+        }
         else {
-            game.initGame(args);
-            System.out.println();
+            System.out.print("""
+                Welcome to the dungeon! Let's begin your adventure...
+                Please increase the console size for a better gaming experience!\n
+                """);
             game.initHero();
             System.out.println();
             game.generateDungeon();
-            System.out.println();
         }
-
 
         System.out.println("Finally we are going into the dungeon! Good luck and have fun!\n");
         while (game.actions() != 0) {
